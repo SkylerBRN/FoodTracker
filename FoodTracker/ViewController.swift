6edd20fd94b81e3666ddc8df699a66b8799bd25a
SkylerBRN <skylerbrn@gmail.com>
2020-05-31 17:27:02 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Skyler Brown on 5/31/20.
+//  Copyright © 2020 Skyler Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
