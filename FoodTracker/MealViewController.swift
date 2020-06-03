5b778e8a21b7bc8d2fe69c3c77e51438fca85187
SkylerBRN <skylerbrn@gmail.com>
2020-06-03 19:49:20 -0400

    Completed 'Create a Table View' section of tutorial, built a custom table view cell, connected data model object to table view controller, added sample data to model, and added code needed to dynamically fill table with data

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Skyler Brown on 5/31/20.
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -34,7 +33,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     func textFieldDidEndEditing(_ textField: UITextField) {
         
         // Set meal name to user's text field input.
-        mealNameLabel.text = textField.text
+
     }
     
     //MARK: UIImagePickerControllerDelegate
